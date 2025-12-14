8d94b309f9c06dc87021b47fdd2b1ba5e7edd6f6
artsy-pl <artsyipl@gmail.com>
2025-12-14 18:56:15 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("tailwindcss-animate") // Optional, adds animation utilities if you install the plugin, otherwise safe to remove if standard animate-in classes work native
+  ],
+}
